d3f6cd1962338322028e1b5098545c14eff0e763
JaiSingleton <single671@wearescpps.org>
2025-11-06 16:03:21 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -7,7 +7,7 @@ var init = function (window) {
         app = window.opspark.makeApp(),
         canvas = app.canvas, 
         view = app.view,
-        fps = draw.fps('#000');
+        fps = draw.fps('#ff0000ff');
         
     
     window.opspark.makeGame = function() {
@@ -20,18 +20,29 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+    var circles = [];
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#3700ffff", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+        } 
 
 
         // TODO 3 : Call the drawCircle() function
-
-
+    drawCircle()
+    drawCircle()
+    drawCircle()
+    drawCircle()
+    drawCircle()
 
         // TODO 7 : Use a loop to create multiple circles
+        for (var i = 0; i < 100; i++) {
+        game.drawCircle(circles[i]); 
+}
 
 
 
@@ -47,15 +58,20 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
+           
+            
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+        
+      
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+           for (var i = 0; i < circles.length; i++) {
+            physikz.updatePosition(circles[1]);
+            game.checkCirclePosition(circles[4]);
+           }
             
-        }
+           }
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -70,7 +86,31 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+        // Right Boundary (example provided in instructions):
+  if (circle.x > CANVAS_WIDTH) {
+    circle.x = 0;
+  }
+
+  // Left Boundary:
+  // When the x position moves off the left side (circle.x < 0),
+  // set circle.x to the right edge (CANVAS_WIDTH).
+  if (circle.x < 0) {
+    circle.x = CANVAS_WIDTH;
+  }
+
+  // Top Boundary:
+  // When the y position moves off the top edge (circle.y < 0),
+  // set circle.y to the bottom edge (CANVAS_HEIGHT).
+  if (circle.y < 0) {
+    circle.y = CANVAS_HEIGHT;
+  }
+
+  // Bottom Boundary:
+  // When the y position moves off the bottom edge (circle.y > CANVAS_HEIGHT),
+  // set circle.y to the top edge (0).
+  if (circle.y > CANVAS_HEIGHT) {
+    circle.y = 0;
+  }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
